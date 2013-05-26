71b93d8bf5d4414e0b045c8cffbe69c0a49a2fb1
walter <walter@nix.(none)>
2013-05-27 00:49:36 +0700

    approvals permissions working

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -3,4 +3,16 @@ module AdminsHelper
 	 def applied_to?(job)
     	JobApplication.where(job_id: job, applicant_id: self).present?    
   	end
+
+  	def is_admin?
+  		if current_employer.present?
+  			if current_employer.admin == true
+  				return true
+  			else
+  				return false
+  			end
+  		end	
+  	end
+  		
+
 end
