854c58823db73fbc27e5571be596ebdea58d16f8
walter <walter@nix.(none)>
2013-06-01 02:19:41 +0700

    layout updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,11 +22,20 @@ Job.create title: "co worker", location: "london", description: "asd", info: "as
 Job.create title: "prostitute", location: "london", description: "asd", info: "asd", employer: a
 
 
- ActsAsTaggableOn::Tag.create name: "Webdesign"
- ActsAsTaggableOn::Tag.create name: "PHP"
- ActsAsTaggableOn::Tag.create name: "Graphical"
- ActsAsTaggableOn::Tag.create name: "Lonix"
- ActsAsTaggableOn::Tag.create name: "bla"
+ActsAsTaggableOn::Tag.create name: "Account Management"
+ActsAsTaggableOn::Tag.create name: "Advertising / Branding"
+ActsAsTaggableOn::Tag.create name: "Business Development"
+ActsAsTaggableOn::Tag.create name: "Communications"
+ActsAsTaggableOn::Tag.create name: "Copywriting / Writing"
+ActsAsTaggableOn::Tag.create name: "Interactive Web / Video"
+ActsAsTaggableOn::Tag.create name: "Marketing / Strategy"
+ActsAsTaggableOn::Tag.create name: "Media Planning / Buying"
+ActsAsTaggableOn::Tag.create name: "Online Marketing"
+ActsAsTaggableOn::Tag.create name: "Public / Investor Relations"
+ActsAsTaggableOn::Tag.create name: "SEO/SEM"
+ActsAsTaggableOn::Tag.create name: "Social Media"
+ActsAsTaggableOn::Tag.create name: "User Experience"
+ActsAsTaggableOn::Tag.create name: "Web Design / Development"
 
 
 
