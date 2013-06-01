948197dc5d3b5bb3b09dbe482856fded323d0b05
walter <walter@nix.(none)>
2013-06-02 02:41:57 +0700

    appds

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -1,8 +1,8 @@
 class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
  
-  def create
-    PaymentNotification.create!(:params => params)
+   def create
+    PaymentNotification.create!(:params => params, :job_id => nil, :status => params[:payment_status], :transaction_id => params[:txn_id])
     render :nothing => true
 
   
