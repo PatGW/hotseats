ef8b9d4f95ad49504aa8453d5094476b3af0fe0e
walter <walter@nix.(none)>
2013-06-02 01:26:12 +0700

    paypal issue

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -2,7 +2,7 @@ class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
  
   def create
-    PaymentNotification.create!(:params => params, :job_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
+    PaymentNotification.create!(:params => "hello", :job_id => params[:invoice], :status => params[:payment_status], :transaction_id => params[:txn_id])
     render :nothing => true
 
   
