c16c8ccdb7392e1d4bbb881222a5c62f4cdd1789
walter <walter@nix.(none)>
2013-06-02 23:50:14 +0700

    added if for payment status

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -4,15 +4,19 @@ class PaymentNotificationsController < ApplicationController
    def create
     PaymentNotification.create!(:params => params, :job_id => params[:item_number], :status => params[:payment_status], :transaction_id => params[:txn_id])
     
+	   	if params[:payment_status] == "Completed"
 
-    @job = Job.find(params[:item_number])
-    @job.update_attributes(:jobpaid => true)
+	   	 	@job = Job.find(params[:item_number])
+	    	@job.update_attributes(:jobpaid => true)
 
-    render :nothing => true
+	    	render :nothing => true
 
+	    else
+	    	render :nothing => true	
+	    end
 
   
-  end
+  	end
 
   
 end
