f5a8609ab433e62c86ce5383bc712f5e8a20c933
walter <walter@nix.(none)>
2013-06-02 18:29:03 +0700

    job ID working

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -2,7 +2,7 @@ class PaymentNotificationsController < ApplicationController
   protect_from_forgery :except => [:create]
  
    def create
-    PaymentNotification.create!(:params => params, :job_id => params[:item_number], :status => "hello", :transaction_id => params[:txn_id])
+    PaymentNotification.create!(:params => params, :job_id => params[:item_number], :status => params[:payment_status], :transaction_id => params[:txn_id])
     render :nothing => true
 
   
