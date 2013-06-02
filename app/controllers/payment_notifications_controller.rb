712f398fb6fff7ad75015d4e8a3e478028880fe1
walter <walter@nix.(none)>
2013-06-02 20:20:29 +0700

    pojl

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -3,11 +3,12 @@ class PaymentNotificationsController < ApplicationController
  
    def create
     PaymentNotification.create!(:params => params, :job_id => params[:item_number], :status => params[:payment_status], :transaction_id => params[:txn_id])
-    render :nothing => true
+    
 
     @job = Job.find(params[:item_number])
     @job.update_attributes(:jobpaid => true)
 
+    render :nothing => true
 
 
   
