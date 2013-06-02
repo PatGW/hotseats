303d67c6fff75a06e45946e03f9b30d64eb1030c
walter <walter@nix.(none)>
2013-06-02 20:25:10 +0700

    hard coded jobid

--- app/controllers/payment_notifications_controller.rb
+++ app/controllers/payment_notifications_controller.rb
@@ -5,8 +5,8 @@ class PaymentNotificationsController < ApplicationController
     PaymentNotification.create!(:params => params, :job_id => params[:item_number], :status => params[:payment_status], :transaction_id => params[:txn_id])
     
 
-    @job = Job.find(params[:item_number])
-    @job.update_attributes(:jobpaid => true)
+    @job = Job.find(40)
+    @job.update_attribute(:jobpaid => true)
 
     render :nothing => true
 
