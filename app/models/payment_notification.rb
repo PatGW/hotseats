9fe4c92f52d829384f281a58ea4e8da956e86b65
walter <walter@nix.(none)>
2013-06-02 18:34:55 +0700

    removed app

--- app/models/payment_notification.rb
+++ app/models/payment_notification.rb
@@ -1,14 +1,13 @@
 class PaymentNotification < ActiveRecord::Base
 	belongs_to :job
 	serialize :params
-	# after_create :mark_job_as_paid
+	after_create :mark_job_as_paid
  	attr_accessible :params, :job_id, :status, :transaction_id
 
  	private
 
  	def mark_job_as_paid
- 		if status == "Completed"
- 			job.update_attribute(:jobpaid => true)
- 		end	
+ 		job.update_attribute(:jobpaid => true)
+ 		
  	end	
 end
