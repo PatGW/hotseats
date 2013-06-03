ea0598035b1a5555f307bfac1589a3e8d7579ac7
walter <walter@nix.(none)>
2013-06-03 22:08:00 +0700

    git add

---
+++ config/initializers/monologue.rb
@@ -0,0 +1,22 @@
+Monologue.sidebar              = ["latest_posts", "latest_tweets"]   # this will add the latests posts and latests tweets in the right sidebar.
+
+Monologue.facebook_url         = "https://www.facebook.com/jipiboilycom" # if set, this will enable Facebook icon and link it to your Facebook page.
+Monologue.google_plus_account_url = "https://plus.google.com/115273180419164295760/posts" # if set, this will enable Google+ icon and link it to that URL.
+
+
+Monologue.facebook_logo           = 'logo.png'  # used in the open graph protocol to display an image when a post is liked
+
+Monologue.layout               = "layouts/monologue/application"
+Monologue.site_name            = "This is my blog"
+Monologue.site_subtitle        = "I write about..."
+Monologue.site_url             = "http://example.com"
+Monologue.disqus_shortname     = "example_disqus_username" # register on disqus.com if you don't have one
+Monologue.meta_description     = "This is the description that will be seen on Google by default for the main page"
+Monologue.meta_keyword         = "those, are, meta, key, words, that, you, should, enter, comma, delimited, just, like, this"
+Monologue.twitter_username     = "twitter_user_name"
+Monologue.twitter_locale       = "en" # This is the locale for twitter. "en" for english or "fr" for french. There is much more!
+Monologue.facebook_like_locale = "en_US" # same, but for facebook. French is "fr_CA"
+Monologue.google_plusone_locale = "en" # same for Google +
+Monologue.admin_force_ssl       = true # that can be set to false if you don't have SSL, but it is not recommended
+Monologue.posts_per_page      = 5 # OPTIONAL: number of posts per page on main page. It defaults to 10 currently
+Monologue.google_analytics_id  = "GA-CODE-HERE" # OPTIONAL: Your Google Analytics code should be here if you have one.
