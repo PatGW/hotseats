8791d4d9b8a5e30ad43e7e9cfd645f62bed23afd
walter <walter@nix.(none)>
2013-06-04 19:58:33 +0700

    searching arrays working and login styling

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -3,8 +3,23 @@ class InfoController < ApplicationController
 
 
   def home
+
+  	if params[:search]
+  		params[:page] == 1
+    	@test = Job.find(:all, :include => :employer, :conditions => ['title LIKE ?', "%#{params[:search]}%"])
+
+    	@jobs = Kaminari.paginate_array(@test).page(params[:page]).per(3)
+
+    else
+
+    	   @jobs = Job.where(:jobpaid => true).order("created_at DESC").page(current_page).per(3)
+
+    	# @jobs = Job.find(:all, :include => :employer)
+    end
+
+
    
-    @jobs = Job.where(:jobpaid => true).order("created_at DESC").page(current_page).per(3)
+
   end
   
   
