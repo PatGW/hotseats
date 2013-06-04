4ff8b7d6c0eda325b129e22a4a207bdfeaf75f20
walter <walter@nix.(none)>
2013-06-05 01:56:59 +0700

    styling changes

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,7 +4,7 @@ class JobApplicationsController < ApplicationController
 
   def create
     JobApplication.create job: Job.find(params[:id]), applicant: current_applicant, cover_letter: "asd"
-    flash[:notice] = "you are applied"
+    flash[:notice] = "You have susessfully applied. Your details have been sent to the employer"
     redirect_to root_path
   end
 
