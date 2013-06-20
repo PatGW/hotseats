92b8b3e633479ac081ebc8352b6f6f756d11bf5e
walter <walter@nix.(none)>
2013-06-21 04:26:21 +0700

    s3

--- config/initializers/a1carrierwave.rb
+++ config/initializers/a1carrierwave.rb
@@ -1,8 +1,11 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AKIAJU4VT7GOBMAASWHQ'],
-    aws_secret_access_key: ENV['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']
-  }
-  config.fog_directory = ENV['hotseatsuploads']
+    config.storage = :fog
+    config.fog_credentials = {
+        provider: 'AWS',
+        aws_access_key_id: 'AKIAJU4VT7GOBMAASWHQ',
+        aws_secret_access_key: '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'
+    }
+
+    config.fog_directory  = 'hotseatsuploads'
+    config.fog_public     = false
 end
