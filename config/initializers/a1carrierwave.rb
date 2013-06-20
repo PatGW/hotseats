ff593b01ce7dea03932a363c4757c98b00899008
walter <walter@nix.(none)>
2013-06-20 21:28:35 +0700

    s3 env variables

---
+++ config/initializers/a1carrierwave.rb
@@ -0,0 +1,8 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AKIAJU4VT7GOBMAASWHQ'],
+    aws_secret_access_key: ENV['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']
+  }
+  config.fog_directory = ENV['AWS_S3_BUCKET']
+end
